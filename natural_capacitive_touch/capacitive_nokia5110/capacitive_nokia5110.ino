4621e416e850ee5da85f5eb30d00790dc362ff6f
Robo Ulbricht <robo.ulbricht@gmail.com>
2016-05-10 19:32:35 +0200

    Capacitive sensor on Nokia 5110

---
+++ natural_capacitive_touch/capacitive_nokia5110/capacitive_nokia5110.ino
@@ -0,0 +1,139 @@
+ /**
+ * Arduino Native Capacitive Sensor
+ * v. 1.0
+ * Copyright (C) 2016 Robert Ulbricht
+ * http://www.arduinoslovakia.eu
+ * http://playground.arduino.cc/Code/CapacitiveSensor
+ *
+ * Capacitive sensor without additional hardware 
+ * on Arduino Uno pin 2.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+uint8_t cs=0;
+
+void drawDisplay() {
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("Capacitive");
+  display.setCursor(0,8);
+  display.print("Sensor");
+
+  display.setCursor(0,32);
+  display.print("MS: ");
+  display.print(millis());
+  display.setCursor(0,40);
+  display.print("CS: ");
+  display.print(cs);
+  display.display(); 
+}
+
+void setup() {
+display.begin();
+display.setContrast(60);
+drawDisplay();
+
+Serial.begin(9600); 
+Serial.println("Arduino Native Capacitive Sensor");
+}
+
+void loop() {
+cs=readCapacitivePin(2);
+Serial.println(cs);
+drawDisplay();
+delay(100);
+}
+
+// readCapacitivePin
+//  Input: Arduino pin number
+//  Output: A number, from 0 to 17 expressing
+//  how much capacitance is on the pin
+//  When you touch the pin, or whatever you have
+//  attached to it, the number will get higher
+
+uint8_t readCapacitivePin(int pinToMeasure) {
+  // Variables used to translate from Arduino to AVR pin naming
+  volatile uint8_t* port;
+  volatile uint8_t* ddr;
+  volatile uint8_t* pin;
+  
+  // Here we translate the input pin number from
+  // Arduino pin number to the AVR PORT, PIN, DDR,
+  // and which bit of those registers we care about.
+  byte bitmask;
+  port = portOutputRegister(digitalPinToPort(pinToMeasure));
+  ddr = portModeRegister(digitalPinToPort(pinToMeasure));
+  bitmask = digitalPinToBitMask(pinToMeasure);
+  pin = portInputRegister(digitalPinToPort(pinToMeasure));
+  
+  // Discharge the pin first by setting it low and output
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+  delay(1);
+  uint8_t SREG_old = SREG; //back up the AVR Status Register
+  
+  // Prevent the timer IRQ from disturbing our measurement
+  noInterrupts();
+  
+  // Make the pin an input with the internal pull-up on
+  *ddr &= ~(bitmask);
+  *port |= bitmask;
+
+  // Now see how long the pin to get pulled up. This manual unrolling of the loop
+  // decreases the number of hardware cycles between each read of the pin,
+  // thus increasing sensitivity.
+  uint8_t cycles = 17;
+  if (*pin & bitmask) { cycles =  0;}
+  else if (*pin & bitmask) { cycles =  1;}
+  else if (*pin & bitmask) { cycles =  2;}
+  else if (*pin & bitmask) { cycles =  3;}
+  else if (*pin & bitmask) { cycles =  4;}
+  else if (*pin & bitmask) { cycles =  5;}
+  else if (*pin & bitmask) { cycles =  6;}
+  else if (*pin & bitmask) { cycles =  7;}
+  else if (*pin & bitmask) { cycles =  8;}
+  else if (*pin & bitmask) { cycles =  9;}
+  else if (*pin & bitmask) { cycles = 10;}
+  else if (*pin & bitmask) { cycles = 11;}
+  else if (*pin & bitmask) { cycles = 12;}
+  else if (*pin & bitmask) { cycles = 13;}
+  else if (*pin & bitmask) { cycles = 14;}
+  else if (*pin & bitmask) { cycles = 15;}
+  else if (*pin & bitmask) { cycles = 16;}
+
+  // End of timing-critical section; turn interrupts back on if they were on before, or leave them off if they were off before
+  SREG = SREG_old;
+
+  // Discharge the pin again by setting it low and output
+  //  It's important to leave the pins low if you want to 
+  //  be able to touch more than 1 sensor at a time - if
+  //  the sensor is left pulled high, when you touch
+  //  two sensors, your body will transfer the charge between
+  //  sensors.
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+
+  return cycles;
+}
