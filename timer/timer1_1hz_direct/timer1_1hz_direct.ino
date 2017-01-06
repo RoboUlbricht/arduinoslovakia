73ca3294b68700b6b73f6af35c20db84f419265d
Robo Ulbricht <robo.ulbricht@gmail.com>
2017-01-06 21:48:08 +0100

    Timer1 blink examples

---
+++ timer/timer1_1hz_direct/timer1_1hz_direct.ino
@@ -0,0 +1,53 @@
+/**
+   Arduino Timer1 CTC 1Hz Interrupt Blink
+   v. 1.0
+   Copyright (C) 2017 Robert Ulbricht
+   http://www.arduinoslovakia.eu
+
+   Blink with interrupts.
+
+   IDE: 1.6.13 or higher
+   Board: Arduino Uno or Pro Mini
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define ledPin 13
+
+void setup()
+{
+  DDRB |= (1<<PB5);
+  
+  // initialize timer1 
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  OCR1A = 15624;            // compare match register 16MHz/256/2Hz
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  interrupts();             // enable all interrupts
+}
+
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+{
+  PORTB ^= (1 << PB5);
+}
+
+void loop()
+{
+  // your program here...
+}
