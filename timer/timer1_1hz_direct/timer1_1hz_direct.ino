e555f264e5605c8777cccf703914c4be674d1f0c
Robo Ulbricht <robo.ulbricht@gmail.com>
2017-01-08 09:25:16 +0100

    Opravy chyb.

--- timer/timer1_1hz_direct/timer1_1hz_direct.ino
+++ timer/timer1_1hz_direct/timer1_1hz_direct.ino
@@ -35,9 +35,9 @@ void setup()
   TCCR1B = 0;
   TCNT1  = 0;
 
-  OCR1A = 15624;            // compare match register 16MHz/256/2Hz
+  OCR1A = 15624;            // compare match register 16MHz/1024/1Hz
   TCCR1B |= (1 << WGM12);   // CTC mode
-  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TCCR1B |= (1 << CS12) | (0 << CS11) | (1 << CS10); // 1024 prescaler 
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
   interrupts();             // enable all interrupts
 }
