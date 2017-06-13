5aad66b51a37510a19a6afe50070ce37e7f78584
Robo Ulbricht <robo.ulbricht@gmail.com>
2017-06-13 09:49:38 +0200

    Extra parameter Arduino Uno

---
+++ extra_parameter/arduino_extra_parameter/arduino_extra_parameter.ino
@@ -0,0 +1,39 @@
+/**
+ * Arduino Uno - Extra parameters
+ * v. 1.0
+ * Copyright (C) 2017 Robert Ulbricht
+ * http://www.arduinoslovakia.eu
+ *
+ * Find file '/hardware/arduino/avr/platform.txt'. In the same directory
+ * create file 'platfom.local.txt'. Insert text 
+ * 'compiler.cpp.extra_flags=-DEXTRA_PARAMETER="abc123"' into the file.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/ .
+ */
+ 
+#if defined(EXTRA_PARAMETER)
+  #define DEPENDENT_PARAMETER "xyz"
+#else
+  #error EXTRA_PARAMETER not found!
+#endif
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(EXTRA_PARAMETER);
+  Serial.println(DEPENDENT_PARAMETER);
+}
+
+void loop() {
+  // nothing
+}
