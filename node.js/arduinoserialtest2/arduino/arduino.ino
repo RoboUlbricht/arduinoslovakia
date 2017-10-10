99627041b2952b2d6a2a7dbd3ad860527c2d4635
Robo Ulbricht <robo.ulbricht@gmail.com>
2017-10-10 21:50:19 +0200

    Node.js Arduino serial port test v.2

---
+++ node.js/arduinoserialtest2/arduino/arduino.ino
@@ -0,0 +1,34 @@
+/**
+   Serial communication between Arduino and Node.js
+   v. 1.0
+   Copyright (C) 2017 Robert Ulbricht
+   https://www.arduinoslovakia.eu
+
+   Arduino sends. Node.js receives.
+
+   IDE: 1.8.3 or higher
+   Board: Arduino Pro Mini
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(millis());
+  delay(1000);
+}
+
