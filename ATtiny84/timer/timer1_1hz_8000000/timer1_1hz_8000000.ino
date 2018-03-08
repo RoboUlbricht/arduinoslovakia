7335254e54788c2c810bccd7751fe6dc3aaba6dd
Robo Ulbricht <robo.ulbricht@gmail.com>
2018-03-08 22:16:37 +0100

    ATtiny84 CTC timer examples

---
+++ ATtiny84/timer/timer1_1hz_8000000/timer1_1hz_8000000.ino
@@ -0,0 +1,61 @@
+/**
+   ATtiny84 Timer1 CTC 1Hz Interrupt Blink
+   v. 1.0
+   Copyright (C) 2018 Robert Ulbricht
+   https://www.arduinoslovakia.eu
+
+   Blink with interrupts.
+
+   IDE: 1.8.4 or higher
+   Board: ATtiny85
+   Core: https://github.com/SpenceKonde/ATTinyCore
+
+   AVR Timer CTC Interrupts Calculator
+   v. 8
+   https://www.arduinoslovakia.eu/application/timer-calculator 
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define ledPin 10
+
+void setupTimer1() {
+  noInterrupts();
+  // Clear registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  // 1 Hz (8000000/((31249+1)*256))
+  OCR1A = 31249;
+  // CTC
+  TCCR1B |= (1 << WGM12);
+  // Prescaler 256
+  TCCR1B |= (1 << CS12);
+  // Output Compare Match A Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  setupTimer1();
+}
+
+void loop() {
+}
+
+ISR(TIMER1_COMPA_vect) {
+  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);
+}
